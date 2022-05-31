efa8bb3ae62c4ea54f030e600375f0393542c3b3
dipper-mizar <1923001710@qq.com>
2022-05-31 20:29:45 +0800

    Provide-a-Cyborg-install-solution

---
+++ Ussuri/OpenStack-Ussuri-Single/iaas-install-cyborg.sh
@@ -0,0 +1,80 @@
+#!/bin/bash
+source openrc.sh
+source /etc/keystone/admin-openrc.sh
+rm -rf ~/cyborg && mkdir ~/cyborg
+cd ~/cyborg
+git clone https://opendev.org/openstack/cyborg -b stable/ussuri
+if [ $? -ne 0 ]; then
+        echo "[ERROR] Clone cyborg failed."
+        rm -rf /root/cyborg
+        exit 1
+fi
+cd ~/cyborg/cyborg
+pip3 install tox
+if [ $? -ne 0 ]; then
+        echo "[ERROR] Install tox failed."
+        exit 1
+fi
+yum -y install gcc
+tox -e genconfig
+cp -r etc/cyborg /etc && cd /etc/cyborg
+ln -s cyborg.conf.sample cyborg.conf
+cd ~/cyborg/cyborg
+python3 setup.py install
+if [ $? -ne 0 ]; then
+        echo "[ERROR] Install package failed."
+        exit 1
+fi
+echo "[INFO] Upgrading SQLAlchemy to void a database migration failure."
+pip3 install --upgrade sqlalchemy
+if [ $? -ne 0 ]; then
+        echo "[ERROR] Upgrade SQLAlchemy failed."
+        exit 1
+fi
+pip3 install python-cyborgclient
+if [ $? -ne 0 ]; then
+        echo "[ERROR] Install Cyborg Python client failed."
+        exit 1
+fi
+rm -rf /root/cyborg
+mysql -uroot -p$DB_PASS -e "create database IF NOT EXISTS cyborg;"
+mysql -uroot -p$DB_PASS -e "GRANT ALL PRIVILEGES ON cyborg.* TO 'cyborg'@'localhost' IDENTIFIED BY '$CYBORG_DBPASS';"
+openstack user create --domain default --password $CYBORG_PASS cyborg
+openstack role add --project service --user cyborg admin
+openstack service create --name cyborg --description "Acceleration Service" accelerator
+openstack endpoint create --region RegionOne accelerator public http://$HOST_NAME:6666/v2
+openstack endpoint create --region RegionOne accelerator internal http://$HOST_NAME:6666/v2
+openstack endpoint create --region RegionOne accelerator admin http://$HOST_NAME:6666/v2
+crudini --set /etc/cyborg/cyborg.conf DEFAULT transport_url rabbit://openstack:$RABBIT_PASS@$HOST_NAME
+crudini --set /etc/cyborg/cyborg.conf DEFAULT use_syslog False
+crudini --set /etc/cyborg/cyborg.conf DEFAULT state_path /var/lib/cyborg
+crudini --set /etc/cyborg/cyborg.conf DEFAULT debug True
+crudini --set /etc/cyborg/cyborg.conf database connection mysql+pymysql://cyborg:$CYBORG_DBPASS@$HOST_NAME/cyborg
+crudini --set /etc/cyborg/cyborg.conf service_catalog cafile /opt/stack/data/ca-bundle.pem
+crudini --set /etc/cyborg/cyborg.conf service_catalog project_domain_id default
+crudini --set /etc/cyborg/cyborg.conf service_catalog user_domain_id default
+crudini --set /etc/cyborg/cyborg.conf service_catalog project_name service
+crudini --set /etc/cyborg/cyborg.conf service_catalog password $CYBORG_PASS
+crudini --set /etc/cyborg/cyborg.conf service_catalog username cyborg
+crudini --set /etc/cyborg/cyborg.conf service_catalog auth_url http://$HOST_NAME/identity
+crudini --set /etc/cyborg/cyborg.conf service_catalog auth_type password
+crudini --set /etc/cyborg/cyborg.conf placement project_domain_name Default
+crudini --set /etc/cyborg/cyborg.conf placement project_name service
+crudini --set /etc/cyborg/cyborg.conf placement user_domain_name Default
+crudini --set /etc/cyborg/cyborg.conf placement password $PLACEMENT_PASS
+crudini --set /etc/cyborg/cyborg.conf placement username placement
+crudini --set /etc/cyborg/cyborg.conf placement auth_url http://$HOST_NAME/identity
+crudini --set /etc/cyborg/cyborg.conf placement auth_type password
+crudini --set /etc/cyborg/cyborg.conf placement auth_section keystone_authtoken
+crudini --set /etc/cyborg/cyborg.conf keystone_authtoken memcached_servers $HOST_NAME:11211
+crudini --set /etc/cyborg/cyborg.conf keystone_authtoken signing_dir /var/cache/cyborg/api
+crudini --set /etc/cyborg/cyborg.conf keystone_authtoken cafile /opt/stack/data/ca-bundle.pem
+crudini --set /etc/cyborg/cyborg.conf keystone_authtoken project_domain_name Default
+crudini --set /etc/cyborg/cyborg.conf keystone_authtoken project_name service
+crudini --set /etc/cyborg/cyborg.conf keystone_authtoken user_domain_name Default
+crudini --set /etc/cyborg/cyborg.conf keystone_authtoken password $CYBORG_PASS
+crudini --set /etc/cyborg/cyborg.conf keystone_authtoken username cyborg
+crudini --set /etc/cyborg/cyborg.conf keystone_authtoken auth_url http://$HOST_NAME/identity
+crudini --set /etc/cyborg/cyborg.conf keystone_authtoken auth_type password
+
+cyborg-dbsync --config-file /etc/cyborg/cyborg.conf upgrade
--- Ussuri/OpenStack-Ussuri-Single/openrc.sh
+++ Ussuri/OpenStack-Ussuri-Single/openrc.sh
@@ -1,75 +1,82 @@
-##--------------------system Config--------------------##
-##Controller Server Manager IP. example:x.x.x.x
-#HOST_IP=
+#--------------------system Config--------------------##
+#Controller Server Manager IP. example:x.x.x.x
+HOST_IP=192.168.26.131
 
-##Controller Server hostname. example:controller
-#HOST_NAME=
+#Controller Server hostname. example:controller
+HOST_NAME=controller
 
-##Compute Node Manager IP. example:x.x.x.x
+#Compute Node Manager IP. example:x.x.x.x
 #HOST_IP_NODE=
 
-##Compute Node hostname. example:compute
+#Compute Node hostname. example:compute
 #HOST_NAME_NODE=
 
-##--------------------Rabbit Config ------------------##
-##user for rabbit. example:openstack
-#RABBIT_USER=
+#--------------------Rabbit Config ------------------##
+#user for rabbit. example:openstack
+RABBIT_USER=openstack
 
-##Password for rabbit user .example:000000
-#RABBIT_PASS=
+#Password for rabbit user .example:000000
+RABBIT_PASS=000000
 
-##--------------------MySQL Config---------------------##
-##Password for MySQL root user . exmaple:000000
-#DB_PASS=
+#--------------------MySQL Config---------------------##
+#Password for MySQL root user . exmaple:000000
+DB_PASS=000000
 
-##--------------------Keystone Config------------------##
-##Password for Keystore admin user. exmaple:000000
-#ADMIN_PASS=
-#DEMO_PASS=
+#--------------------Keystone Config------------------##
+#Password for Keystore admin user. exmaple:000000
+ADMIN_PASS=000000
+DEMO_PASS=000000
 
-##Password for Mysql keystore user. exmaple:000000
-#KEYSTONE_DBPASS=
+#Password for Mysql keystore user. exmaple:000000
+KEYSTONE_DBPASS=000000
 
-##--------------------Glance Config--------------------##
-##Password for Mysql glance user. exmaple:000000
-#GLANCE_DBPASS=
+#--------------------Glance Config--------------------##
+#Password for Mysql glance user. exmaple:000000
+GLANCE_DBPASS=000000
 
-##Password for Keystore glance user. exmaple:000000
-#GLANCE_PASS=
+#Password for Keystore glance user. exmaple:000000
+GLANCE_PASS=000000
 
-##--------------------Placement Config----------------------##
-##Password for Mysql placement user. exmaple:000000
-#PLACEMENT_DBPASS=
+#--------------------Placement Config----------------------##
+#Password for Mysql placement user. exmaple:000000
+PLACEMENT_DBPASS=000000
 
-##Password for Keystore placement user. exmaple:000000
-#PLACEMENT_PASS=
+#Password for Keystore placement user. exmaple:000000
+PLACEMENT_PASS=000000
 
-##--------------------Nova Config----------------------##
-##Password for Mysql nova user. exmaple:000000
-#NOVA_DBPASS=
+#--------------------Nova Config----------------------##
+#Password for Mysql nova user. exmaple:000000
+NOVA_DBPASS=000000
 
-##Password for Keystore nova user. exmaple:000000
-#NOVA_PASS=
+#Password for Keystore nova user. exmaple:000000
+NOVA_PASS=000000
 
-##--------------------Neturon Config-------------------##
-##Password for Mysql neutron user. exmaple:000000
-#NEUTRON_DBPASS=
+#--------------------Neturon Config-------------------##
+#Password for Mysql neutron user. exmaple:000000
+NEUTRON_DBPASS=000000
 
-##Password for Keystore neutron user. exmaple:000000
-#NEUTRON_PASS=
+#Password for Keystore neutron user. exmaple:000000
+NEUTRON_PASS=000000
 
-##metadata secret for neutron. exmaple:000000
-#METADATA_SECRET=
+#metadata secret for neutron. exmaple:000000
+METADATA_SECRET=000000
 
-##External Network Interface. example:ens34
-#INTERFACE_NAME=
+#External Network Interface. example:ens34
+INTERFACE_NAME=ens192
 
-##--------------------Cinder Config--------------------##
-##Password for Mysql cinder user. exmaple:000000
-#CINDER_DBPASS=
+#--------------------Cinder Config--------------------##
+#Password for Mysql cinder user. exmaple:000000
+CINDER_DBPASS=000000
 
-##Password for Keystore cinder user. exmaple:000000
-#CINDER_PASS=
+#Password for Keystore cinder user. exmaple:000000
+CINDER_PASS=000000
 
-##Cinder Block Disk. example:md126p3
-#BLOCK_DISK=
+#--------------------Cyborg Config--------------------##
+#Password for Mysql cyborg user. example:000000
+CYBORG_DBPASS=000000
+
+#Password for Keystone cyborg user. example:000000
+CYBORG_PASS=000000
+
+#Cinder Block Disk. example:md126p3
+BLOCK_DISK=nvme0n2
