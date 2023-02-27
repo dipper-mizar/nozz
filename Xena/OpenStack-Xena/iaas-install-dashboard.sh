4123cfa3caf575092c81fc1aea300e1b0be71874
zhong.zhou <zhong.zhou@easystack.cn>
2023-02-28 01:42:45 +0800

    Provide Xena double nodes solution

---
+++ Xena/OpenStack-Xena/iaas-install-dashboard.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+# 加载环境变量
+source openrc.sh
+# 安装软件包
+yum -y install openstack-dashboard
+# 主配置文件设置
+sed -i "/^OPENSTACK_HOST =/cOPENSTACK_HOST = \"$HOST_NAME\"" /etc/openstack-dashboard/local_settings
+sed -i "/^ALLOWED_HOSTS =/cALLOWED_HOSTS = ['*','localhost']" /etc/openstack-dashboard/local_settings
+sed -i "/^#SESSION_ENGINE =/cSESSION_ENGINE = 'django.contrib.sessions.backends.cache'" /etc/openstack-dashboard/local_settings
+sed -i "/^TIME_ZONE =/cTIME_ZONE=\"Asia/Shanghai\"" /etc/openstack-dashboard/local_settings
+sed -i "/#CACHES = {/i\CACHES = {\n    'default': {\n        'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',\n        'LOCATION': '$HOST_NAME:11211',\n    },\n}" /etc/openstack-dashboard/local_settings
+sed -i '/OPENSTACK_KEYSTONE_URL = /aOPENSTACK_KEYSTONE_MULTIDOMAIN_SUPPORT = True' /etc/openstack-dashboard/local_settings
+sed -i '/OPENSTACK_KEYSTONE_MULTIDOMAIN_SUPPORT = /aOPENSTACK_API_VERSIONS = {\n    \"identity\": 3,\n    \"image\": 2,\n    \"volume\": 3,\n}' /etc/openstack-dashboard/local_settings
+sed -i '/OPENSTACK_KEYSTONE_MULTIDOMAIN_SUPPORT = /aOPENSTACK_KEYSTONE_DEFAULT_DOMAIN = \"Default\"\nOPENSTACK_KEYSTONE_DEFAULT_ROLE = \"user\"' /etc/openstack-dashboard/local_settings
+sed -i "/WSGISocketPrefix run\/wsgi/a\WSGIApplicationGroup %{GLOBAL}" /etc/httpd/conf.d/openstack-dashboard.conf
+ln -s /etc/openstack-dashboard /usr/share/openstack-dashboard/openstack_dashboard/conf
+sed -i "/^WEBROOT = /cWEBROOT = '/dashboard'  # from openstack_auth" /usr/share/openstack-dashboard/openstack_dashboard/defaults.py
+sed -i "/WEBROOT = /cWEBROOT = '/dashboard'" /usr/share/openstack-dashboard/openstack_dashboard/test/settings.py
+sed -i "/ExecCGI/aRequire all granted" /usr/share/keystone/wsgi-keystone.conf
+
+mkdir /usr/share/openstack-dashboard/openstack_dashboard/wsgi
+cat >> /usr/share/openstack-dashboard/openstack_dashboard/wsgi/django.wsgi << EOF
+# Copyright (c) 2017 OpenStack Foundation.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""
+WSGI config for openstack_dashboard project.
+"""
+
+import logging
+import os
+import sys
+
+from django.core.wsgi import get_wsgi_application
+
+# Add this file path to sys.path in order to import settings
+sys.path.insert(0, os.path.normpath(os.path.join(
+    os.path.dirname(os.path.realpath(__file__)), '../..')))
+os.environ['DJANGO_SETTINGS_MODULE'] = 'openstack_dashboard.settings'
+sys.stdout = sys.stderr
+
+logging.warning(
+    "Use of this 'djano.wsgi' file has been deprecated since the Rocky "
+    "release in favor of 'wsgi.py' in the 'openstack_dashboard' module. This "
+    "file is a legacy naming from before Django 1.4 and an importable "
+    "'wsgi.py' is now the default. This file will be removed in the T release "
+    "cycle."
+)
+
+application = get_wsgi_application()
+EOF
+
+# 重启服务
+systemctl restart httpd.service memcached.service
---
+++ Xena/OpenStack-Xena/iaas-install-nova-compute.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+source openrc.sh
+function correct_repo() {
+    sed -i -e "s|mirrorlist=|#mirrorlist=|g" /etc/yum.repos.d/CentOS-*
+    sed -i -e "s|#baseurl=http://mirror.centos.org|baseurl=https://mirrors.aliyun.com|g" /etc/yum.repos.d/CentOS-*
+}
+# This version Nova need openvswitch package, but the version release
+# repo didn't have ovs, just replace it to Ussuri.
+yum -y install epel-release
+yum -y install centos-release-openstack-ussuri
+correct_repo
+sed -i "9d" /etc/yum.repos.d/CentOS-Ceph-Nautilus.repo
+ceph_nautilus_baseurl_entry="baseurl=https://mirrors.aliyun.com/\$contentdir/\$avstream/storage/\$basearch/ceph-nautilus/"
+sed -i "/\[centos-ceph-pacific\]/a$ceph_nautilus_baseurl_entry" /etc/yum.repos.d/CentOS-Ceph-Nautilus.repo
+
+# 安装Nova需要的软件包
+yum -y install openstack-nova-compute
+crudini --set /etc/nova/nova.conf DEFAULT enabled_apis osapi_compute,metadata
+crudini --set /etc/nova/nova.conf DEFAULT compute_driver libvirt.LibvirtDriver
+# 连接消息队列
+crudini --set /etc/nova/nova.conf DEFAULT transport_url rabbit://openstack:$RABBIT_PASS@$HOST_NAME
+# 配置Keystone认证
+crudini --set /etc/nova/nova.conf api auth_strategy keystone
+crudini --set /etc/nova/nova.conf keystone_authtoken www_authenticate_uri http://$HOST_NAME:5000/
+crudini --set /etc/nova/nova.conf keystone_authtoken auth_url http://$HOST_NAME:5000/
+crudini --set /etc/nova/nova.conf keystone_authtoken memcached_servers $HOST_NAME:11211
+crudini --set /etc/nova/nova.conf keystone_authtoken auth_type password
+crudini --set /etc/nova/nova.conf keystone_authtoken project_domain_name Default
+crudini --set /etc/nova/nova.conf keystone_authtoken user_domain_name Default
+crudini --set /etc/nova/nova.conf keystone_authtoken project_name service
+crudini --set /etc/nova/nova.conf keystone_authtoken username nova
+crudini --set /etc/nova/nova.conf keystone_authtoken password $NOVA_PASS
+crudini --set /etc/nova/nova.conf DEFAULT my_ip $HOST_IP_NODE
+crudini --set /etc/nova/nova.conf DEFAULT use_neutron true
+crudini --set /etc/nova/nova.conf DEFAULT firewall_driver nova.virt.firewall.NoopFirewallDriver
+# 配置VNC连接
+crudini --set /etc/nova/nova.conf vnc enabled true
+crudini --set /etc/nova/nova.conf vnc vncserver_listen 0.0.0.0
+crudini --set /etc/nova/nova.conf vnc vncserver_proxyclient_address $HOST_IP_NODE
+crudini --set /etc/nova/nova.conf vnc novncproxy_base_url  http://$HOST_IP:6080/vnc_auto.html
+crudini --set /etc/nova/nova.conf glance api_servers http://$HOST_NAME:9292
+crudini --set /etc/nova/nova.conf oslo_concurrency lock_path /var/lib/nova/tmp
+# 配置连接Placement认证
+crudini --set /etc/nova/nova.conf placement region_name RegionOne
+crudini --set /etc/nova/nova.conf placement project_domain_name DEFAULT
+crudini --set /etc/nova/nova.conf placement project_name service
+crudini --set /etc/nova/nova.conf placement auth_type password
+crudini --set /etc/nova/nova.conf placement user_domain_name DEFAULT
+crudini --set /etc/nova/nova.conf placement auth_url http://$HOST_NAME:5000/v3
+crudini --set /etc/nova/nova.conf placement username placement
+crudini --set /etc/nova/nova.conf placement password $PLACEMENT_PASS
+crudini --set /etc/nova/nova.conf libvirt virt_type qemu
+crudini --set /etc/nova/nova.conf libvirt inject_key True
+iptables -F
+iptables -X
+iptables -Z
+/usr/libexec/iptables/iptables.init save
+# 落定服务
+systemctl enable libvirtd.service openstack-nova-compute.service
+systemctl start libvirtd.service openstack-nova-compute.service
